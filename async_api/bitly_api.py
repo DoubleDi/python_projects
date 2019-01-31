80ae23e7d5b45471998833ebfa6de0cecc2139d6
Denis Dorozhkin <d.dorogkin@corp.mail.ru>
2019-01-31 16:00:02 +0300

    correct async api

--- async_api/bitly_api.py
+++ async_api/bitly_api.py
@@ -2,6 +2,7 @@ import asyncio
 import aiohttp
 from base64 import b64encode
 from collections import namedtuple
+import json
 import logging
 
 logging.basicConfig(format='%(asctime)s [%(levelname)s]: %(message)s')
@@ -20,32 +21,36 @@ class APIException(Exception):
 
 class BitlyLinkAPI(object):
 
-    def __init__(self, access_token=None):
-        asyncio.get_event_loop()
-        self.session = aiohttp.ClientSession()
+    def __init__(self, session, access_token=None):
+        self.session = session
         self.access_token = access_token
         self.base_url = "https://api-ssl.bitly.com"
+        self.uri = []
 
-    def __build_url(self, link):
-        if not link.startswith("http"):
-            link = f"http://bit.ly/{link}"
-        return f"{self.base_url}/v3/link/clicks?access_token={self.access_token}&link={link}"
+    def __build_url(self):
+        path = "/".join(self.uri)
+        self.uri = []
+        return f"{self.base_url}/v3/{path}"
 
-    async def clicks(self, link=None):
-        logger.info(f"GET clicks for url {link}")
-        url = self.__build_url(link)
+    def __getattr__(self, attr):
+        self.uri.append(attr)
+        return self
+
+    async def __call__(self, *args, **kwargs):
+        logger.info(f"Request method {self.uri} with args {kwargs}")
+        url = self.__build_url()
+        kwargs['access_token'] = self.access_token
 
         logger.debug(f"Request url {url}")
-        async with self.session.get(url) as response:
+        async with self.session.get(url, params=kwargs) as response:
             if response.status != 200:
                 raise APIException(response.status, 'BITLY API ERROR')
             json_response = await response.json()
             if json_response.get('data', None) is None:
-                raise APIException(404, 'NOT FOUND')
-            return namedtuple('response', json_response['data'].keys())(**json_response['data'])
+                raise APIException(json_response['status_code'], json_response['status_txt'])
 
-    async def close(self):
-        await self.session.close()
+            response_tuple = namedtuple('response', json_response['data'].keys())
+            return response_tuple(**json_response['data'])
 
 
 class BitlyAPI(object):
@@ -57,25 +62,34 @@ class BitlyAPI(object):
         self.token = token
 
     async def __aenter__(self):
+        self.session = aiohttp.ClientSession()
+
         if self.token is None:
             url = 'https://api-ssl.bitly.com/oauth/access_token'
-            headers = {'Authorization': 'Basic ' + b64encode(f'{self.username}:{self.password}'.encode()).decode()}
-            async with aiohttp.ClientSession(headers=headers) as session:
-                async with session.post(url) as response:
-                    self.token = await response.text()
-
-
-        self.link = BitlyLinkAPI(self.token)
+            base64_token = b64encode(f'{self.username}:{self.password}'.encode()).decode()
+            headers = {'Authorization': 'Basic ' + base64_token}
+            async with self.session.post(url, headers=headers) as response:
+                text = await response.text()
+                try:
+                    bad_response = json.loads(text)
+                    raise APIException(bad_response['status_code'], bad_response['status_txt'])
+                except json.JSONDecodeError:
+                    self.token = text
 
         return self
 
+    def __getattr__(self, attr):
+        api = BitlyLinkAPI(self.session, self.token)
+        return getattr(api, attr)
+
+
     async def __aexit__(self, exc_type, exc, tb):
-        await self.link.close()
+        await self.session.close()
 
 
 async def call_bitly_api(**credentials):
     async with BitlyAPI(**credentials) as api:
-        links = ['http://bit.ly/2RSlC59', '2RSlC59']
+        links = ['http://bit.ly/2RSlC59', 'http://bit.ly/2RSlC59']
         try:
             responses = await asyncio.gather(
                 *[api.link.clicks(link=link) for link in links],
@@ -83,6 +97,16 @@ async def call_bitly_api(**credentials):
 
             for link, response in zip(links, responses):
                 logger.info(f"{link} has {response.link_clicks} clicks")  # output: <number of clicks>
+        except APIException as err:
+            logger.warn(err)
+        try:
+            responses = await asyncio.gather(
+                api.link.countries(link='http://bit.ly/2RSlC59'),
+                api.user.clicks(),
+            )
+
+            logger.info(f"http://bit.ly/2RSlC59 has {responses[0]}")
+            logger.info(f"user has {responses[1]}")
         except APIException as err:
             logger.warn(err)
 
